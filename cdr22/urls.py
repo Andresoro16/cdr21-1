74a28c05e9caa2285fd90d484ec1ac89755997dd
Cotopaco1 <cotopaco123@hotmail.es>
2025-11-20 21:09:12 -0500

    Add invitado and routes

--- cdr22/urls.py
+++ cdr22/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from . import views
+
+""" path = '' """
 urlpatterns =[
     path ('',views.principal, name='principal'),
-    
+    path ('login', views.login, name='login'),
 ]
+
--- cdr22/views.py
+++ cdr22/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
+
 def principal (request):
-    return render(request, 'dasboard/principal.html')
+    return render(request, 'dashboard/principal.html')
+
+def login(request):
+    return render(request, 'invitado/login.html')
 
 # Create your views here.
