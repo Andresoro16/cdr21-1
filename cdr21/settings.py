962c66132d823be1a3aa2bfb6c7395a7ded171d7
Cotopaco1 <cotopaco123@hotmail.es>
2025-11-26 20:01:25 -0500

    Update database settings to use MySQL and enhance CSS styles for improved layout and design

--- cdr21/settings.py
+++ cdr21/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'cdr21.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'cdr21',
+        'USER' : 'root',
+        'PASSWORD' : 'sachiysergio',
+        'HOST' : 'localhost',
+        'PORT' : '3306'
     }
 }
 
