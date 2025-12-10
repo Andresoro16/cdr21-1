d3430f2ed9847d0359154e814b052d5e25a4b86e
Cotopaco1 <cotopaco123@hotmail.es>
2025-12-09 21:50:55 -0500

    feat : add olvide-password view

--- cdr22/urls.py
+++ cdr22/urls.py
@@ -5,7 +5,7 @@ from . import views
 urlpatterns =[
     path ('',views.principal, name='principal'),
     path ('login', views.login, name='login'),
-
     path ('home', views.home, name='home'),
+    path ('olvide-password', views.olvidePassword, name='olvide-password')
 ]
 
